9dbe48a2d29c47b8b46ef1972c9b741502287061
kimberlyma10 <mak3225@my.sduhsd.net>
2026-04-07 22:19:12 +0000

    hi

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // add javascript here
+let guess = 0;
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+const scores = [];
+
+document.getElementById("playBtn").addEventListener
+("click", play);
+function play({
+    let range = 0;
+    let levels = document.getElementsByName("level");
+    for(let i=0; i<levels.length; i++){
+        if(levels[i].checked){
+            range = parseInt(levels[i].value);
+        }
+    }
+
+    document.getElementById("msg").textContent = 
+    "Guess a number 1-" + Range;
+
+}
