4b828f73ab7040491b3348b4562847368ee1245b
kimberlyma10 <mak3225@my.sduhsd.net>
2026-04-08 22:02:40 +0000

    ui

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let totalWins = 0;
 const scores = [];
 
 document.getElementById("playBtn").addEventListener
+document.getElementById("guessBtn").addEventListener("click", makeGuess);
 ("click", play);
 function play(){
+    
     let range = 0;
     let levels = document.getElementsByName("level");
     for(let i=0; i<levels.length; i++){
@@ -26,4 +28,30 @@ function play(){
     giveUpBtn.disabled = false;
     playBtn.disabled = true;
 
+
+}
+function makeGuess(){
+    let guess = parseInt(document.getElementById("guess").value);
+    if(isNaN(guess)){
+        msg.textContent = "Please enter a valid number";
+        return;
+    }
+    guessCount++;
+    if(guess == answer){
+        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        updateScore(guessCount);
+        reset();
+    }
+    else if (guess < answer){
+        msg.textcontent = "Too low, try again!";
+    else{
+        msg.textContent = "Too high, try again!";
+    }
+
+    }
+}
+function updateScore(score){
+    totalWins++;
+    scores.push(score);
+    wins.textContent = "Total wins: " + totalWins;
 }
